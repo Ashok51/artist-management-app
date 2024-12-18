ff9ca9262408bf38cfbf629067920362b983a7e9
Ashok Poudel <ashokpoudel2051@gmail.com>
2024-12-18 23:17:36 +0545

    flow to add new user by a user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,5 +3,34 @@ module Admin
     def index
       @users = User.all
     end
+
+    def new
+      @user = User.new
+    end
+
+    def create
+      @user = User.new(user_params)
+
+      add_password_to_params
+
+      if @user.save!
+        redirect_to admin_users_path, notice: 'User was successfully created.'
+      else
+        render :new, alert: 'Failed to create the user.'
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :phone, :date_of_birth, :gender, :address,
+                               :email, :role)
+    end
+
+    def add_password_to_params
+      generated_password = SecureRandom.hex(8)
+      @user.password = generated_password
+      @user.password_confirmation = generated_password
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def humanized_roles
+    User.roles.keys.map do |role|
+      [role.split('_').map(&:capitalize).join(' '), role]
+    end
+  end
+
+  def humanized_gender_names
+    User.genders.keys.map { |gender| [gender.capitalize, gender] }
+  end
 end
