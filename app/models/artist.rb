e4c44175c20eac95c3cde9c4cd82e6646052b99d
Ashok Poudel <ashokpoudel2051@gmail.com>
2024-12-19 14:56:33 +0545

    add artist model with its validations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 class Artist < ApplicationRecord
+  validates :first_name, :last_name,
+            :date_of_birth, :gender,
+            :address, presence: true
+
+  validates :first_released_year, :number_of_albums_released,
+            numericality: { only_integer: true,
+                            greater_than_or_equal_to: 0 }, allow_nil: true
 end
